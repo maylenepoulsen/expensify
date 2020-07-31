a20e131c39b1ecd54602f73e557c1dcc537a8925
maylenepoulsen <mayleneski@hotmail.com>
2020-07-31 15:27:03 -0500

    initial commit

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NotFoundPage = () => (
+  <div>
+    This is the 404 page component. <Link to='/'>Back to Home Page</Link>
+  </div>
+)
+
+export default NotFoundPage
